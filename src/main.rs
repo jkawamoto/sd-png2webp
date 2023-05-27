02c11bfb3c6422582d4ba1cccea7c56d24125325
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-05-26 22:26:58 -0600

    feat: multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
+use crossbeam::sync::WaitGroup;
+use scheduled_thread_pool::ScheduledThreadPool;
 
 mod exif;
 mod image;
@@ -19,14 +21,13 @@ fn convert(p: &Path) -> Result<()> {
 
 /// Converts the given file in WebP image. If the given path represents a directory,
 /// this function walks into the directory and converts all images.
-fn convert_all<T: AsRef<Path>>(p: T) {
-    let p = p.as_ref();
+fn convert_all(p: PathBuf, pool: &ScheduledThreadPool, wg: &WaitGroup) {
     if p.is_dir() {
         match p.read_dir() {
             Ok(v) => {
                 for r in v {
                     match r {
-                        Ok(e) => convert_all(e.path()),
+                        Ok(e) => convert_all(e.path(), pool, wg),
                         Err(e) => println!("failed to read a directory: {}", e)
                     }
                 }
@@ -39,16 +40,24 @@ fn convert_all<T: AsRef<Path>>(p: T) {
     if p.extension().unwrap_or("".as_ref()) != "png" {
         return;
     }
-    if let Err(e) = convert(p) {
-        println!("failed to convert {}: {}", p.to_string_lossy(), e);
-    }
+
+    let wg = wg.clone();
+    _ = pool.execute(move || {
+        if let Err(e) = convert(&p) {
+            println!("failed to convert {}: {}", p.to_string_lossy(), e);
+        }
+        drop(wg);
+    })
 }
 
 
 fn main() {
+    let wg = WaitGroup::new();
+    let pool = ScheduledThreadPool::new(num_cpus::get() / 2 + 1);
     for p in env::args().skip(1) {
-        convert_all(p);
+        convert_all(PathBuf::from(p), &pool, &wg);
     }
+    wg.wait();
 }
 
 
